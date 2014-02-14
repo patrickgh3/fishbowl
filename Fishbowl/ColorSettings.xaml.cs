86f2feffdde2ea59b7dac9059f43afa0ca2cdb74
Patrick Traynor <patrickjtraynor@gmail.com>
2014-02-13 21:10:14 -0800

    Basic color selecting
    Only sets the background color.

--- Fishbowl/ColorSettings.xaml.cs
+++ Fishbowl/ColorSettings.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI;
 
 // The Settings Flyout item template is documented at http://go.microsoft.com/fwlink/?LinkId=273769
 
@@ -19,30 +20,63 @@ namespace Fishbowl
 {
     public sealed partial class ColorSettings : SettingsFlyout
     {
+        public static Color BackgroundColor;
         private bool pressed;
+        private double sat;
+        private double val;
 
         public ColorSettings()
         {
             this.InitializeComponent();
+            BackgroundColor = new Color()
+            {
+                R = 0xdd,
+                G = 0xdd,
+                B = 0xdd,
+                A = 0xff
+            };
         }
 
         public void SetControlsToDefaults()
         {
-            // TODO: this
+            sat = 1;
+            val = 1;
+            ColorSelectorHueSlider.Value = 0;
+        }
+
+        // color related
+
+        private void UpdateColor()
+        {
+            byte r, g, b;
+            FishUtil.HsvToRgb(ColorSelectorHueSlider.Value, sat, val, out r, out g, out b);
+            BackgroundColor.R = r;
+            BackgroundColor.G = g;
+            BackgroundColor.B = b;
+            ((SolidColorBrush)BubbleContainer.canvas.Background).Color = BackgroundColor;
+
+            FishUtil.HsvToRgb(ColorSelectorHueSlider.Value, 1, 1, out r, out g, out b);
+            ((SolidColorBrush)ColorSelectorHueRect.Fill).Color = new Color() { R = r, G = g, B = b, A = 0xff };
         }
 
         private void ColorSelectorHueSlider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
         {
-            // TODO: this
+            UpdateColor();
         }
 
         private void SettingsFlyout_PointerMoved(object sender, PointerRoutedEventArgs e)
         {
             if (!pressed) return;
             Point p = e.GetCurrentPoint(ColorSelectorCanvas).Position;
-            Canvas.SetLeft(ColorSelectorEllipse, FishUtil.ClampDouble(p.X, 0, 256) - ColorSelectorEllipse.Width / 2);
-            Canvas.SetTop(ColorSelectorEllipse, FishUtil.ClampDouble(p.Y, 0, 256) - ColorSelectorEllipse.Height / 2);
+            p = new Point(FishUtil.ClampDouble(p.X, 0, 256), FishUtil.ClampDouble(p.Y, 0, 256));
+            Canvas.SetLeft(ColorSelectorEllipse, p.X - ColorSelectorEllipse.Width / 2);
+            Canvas.SetTop(ColorSelectorEllipse, p.Y - ColorSelectorEllipse.Height / 2);
+
+            FishUtil.PosToHsv((int)p.X, (int)p.Y, out sat, out val);
+            UpdateColor();
         }
+
+        // non-color releated
 
         private void Rectangle_PointerPressed(object sender, PointerRoutedEventArgs e)
         {
--- Fishbowl/FishUtil.cs
+++ Fishbowl/FishUtil.cs
@@ -25,5 +25,126 @@ namespace Fishbowl
             if (n > high) n = high;
             return n;
         }
+
+        // Taken from http://www.splinter.com.au/converting-hsv-to-rgb-colour-using-c/
+
+        /// <summary>
+        /// Convert HSV to RGB
+        /// h is from 0-360
+        /// s,v values are 0-1
+        /// r,g,b values are 0-255
+        /// Based upon http://ilab.usc.edu/wiki/index.php/HSV_And_H2SV_Color_Space#HSV_Transformation_C_.2F_C.2B.2B_Code_2
+        /// </summary>
+        public static void HsvToRgb(double h, double S, double V, out byte r, out byte g, out byte b)
+        {
+            double H = h;
+            while (H < 0) { H += 360; };
+            while (H >= 360) { H -= 360; };
+            double R, G, B;
+            if (V <= 0)
+            { R = G = B = 0; }
+            else if (S <= 0)
+            {
+                R = G = B = V;
+            }
+            else
+            {
+                double hf = H / 60.0;
+                int i = (int)Math.Floor(hf);
+                double f = hf - i;
+                double pv = V * (1 - S);
+                double qv = V * (1 - S * f);
+                double tv = V * (1 - S * (1 - f));
+                switch (i)
+                {
+
+                    // Red is the dominant color
+
+                    case 0:
+                        R = V;
+                        G = tv;
+                        B = pv;
+                        break;
+
+                    // Green is the dominant color
+
+                    case 1:
+                        R = qv;
+                        G = V;
+                        B = pv;
+                        break;
+                    case 2:
+                        R = pv;
+                        G = V;
+                        B = tv;
+                        break;
+
+                    // Blue is the dominant color
+
+                    case 3:
+                        R = pv;
+                        G = qv;
+                        B = V;
+                        break;
+                    case 4:
+                        R = tv;
+                        G = pv;
+                        B = V;
+                        break;
+
+                    // Red is the dominant color
+
+                    case 5:
+                        R = V;
+                        G = pv;
+                        B = qv;
+                        break;
+
+                    // Just in case we overshoot on our math by a little, we put these here. Since its a switch it won't slow us down at all to put these here.
+
+                    case 6:
+                        R = V;
+                        G = tv;
+                        B = pv;
+                        break;
+                    case -1:
+                        R = V;
+                        G = pv;
+                        B = qv;
+                        break;
+
+                    // The color is not defined, we should throw an error.
+
+                    default:
+                        //LFATAL("i Value error in Pixel conversion, Value is %d", i);
+                        R = G = B = V; // Just pretend its black/white
+                        break;
+                }
+            }
+            r = (byte)(Clamp((int)(R * 255.0)));
+            g = (byte)(Clamp((int)(G * 255.0)));
+            b = (byte)(Clamp((int)(B * 255.0)));
+        }
+
+        private static int Clamp(int i)
+        {
+            if (i < 0) return 0;
+            if (i > 255) return 255;
+            return i;
+        }
+
+        // http://www.switchonthecode.com/tutorials/javascript-interactive-color-picker
+
+        public static void PosToHsv(int x, int y, out double s, out double v)
+        {
+            s = 1 - (double)y / 255.0;
+            v = (double)x / 255.0;
+        }
+
+        public static void HsvToPos(double s, double v, out int x, out int y)
+        {
+            x = (int)(v * 255);
+            y = (int)((1 - s) * 255);
+        }
     }
 }
